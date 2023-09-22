ac7533c80e3aeb91643c0215fee41a78b527e786
Courtney James McCracken II <courtmac.ii@icloud.com>
2023-09-22 12:53:05 -0400

    Controller Progress
    Yeah

--- Assets/Scripts/PlayerScripts/PlayerController.cs
+++ Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -14,8 +14,15 @@ public class PlayerController : MonoBehaviour
 
     private CharacterController characterController;
 
+    private float horizontalVelocity;
+    private float gravity = -9.81f;
+
+    private float velocity;
+
     //Player Parameters
+    [SerializeField] private float rotationTime = 0.05f;
     [SerializeField] private float speed;
+    [SerializeField] private float gravityMultiplier = 3.0f;
 
     private void Awake()
     {
@@ -24,9 +31,43 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
+        ApplyGravity();
+        ApplyRotation();
+        ApplyMovement();
+    }
+
+    private void ApplyGravity()
+    {
+        if (characterController.isGrounded && velocity < 0.0f)
+        {
+            velocity = -1.0f;
+        }
+        else
+        {
+            velocity += gravity * gravityMultiplier * Time.deltaTime;
+        }
+        direction.y = velocity;
+    }
+
+    private void ApplyRotation()
+    {
+        //if the player is not inputting anything, do not update player's rotation
+        if (input.sqrMagnitude == 0) return;
+
+        //this chunk of code rotates the direction the player is facing
+        var targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+        var angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref horizontalVelocity, rotationTime); ;
+        transform.rotation = Quaternion.Euler(0.0f, angle, 0.0f);
+    }
+
+    private void ApplyMovement()
+    {
+        //this line moves the player
         characterController.Move(direction * speed * Time.deltaTime);
     }
 
+    /*This function is called by the "PlayerInput" Object to move the player
+    based on Unity's input system*/
     public void Move(InputAction.CallbackContext context)
     {
         input = context.ReadValue<Vector2>();
