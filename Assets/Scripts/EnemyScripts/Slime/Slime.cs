1e4f294edc56798415f271fa942b2d4f5f7bfed8
Michael Lowry <michael.lowry64@gmail.com>
2023-10-03 00:12:37 -0400

    new slime script pt. 1

---
+++ Assets/Scripts/EnemyScripts/Slime/Slime.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+public class Slime : Enemy
+{
+    public float moveSpeed = 5f;
+    public LayerMask groundLayer;  // LayerMask to identify ground objects
+    private GameObject player;
+    private Camera mainCamera;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        mainCamera = Camera.main;
+    }
+
+    void FixedUpdate()
+    {
+        SlideTowardsPlayer();
+    }
+
+    void Update()
+    {
+        FaceCamera();
+    }
+
+    private void SlideTowardsPlayer()
+    {
+        // Move towards the player only if the player exists and the slime is grounded
+        if (player != null && IsGrounded())
+        {
+            Vector3 direction = player.transform.position - transform.position;
+            Vector3 horizontalDirection = new Vector3(direction.x, 0, direction.z).normalized;
+            rb.velocity = horizontalDirection * moveSpeed;
+        }
+    }
+
+    private void FaceCamera()
+    {
+        // Ensure the slime always faces the camera
+        if (mainCamera != null)
+        {
+            Vector3 lookDirection = mainCamera.transform.position - transform.position;
+            lookDirection.y = 0;  // Maintain upright orientation
+            Quaternion rotation = Quaternion.LookRotation(lookDirection);
+            transform.rotation = rotation;
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        Debug.Log(Physics.Raycast(transform.position, Vector3.down, 0.1f, groundLayer));
+        // Check if the slime is grounded using raycasting
+        return Physics.Raycast(transform.position, Vector3.down, 0.1f, groundLayer);
+    }
+}
