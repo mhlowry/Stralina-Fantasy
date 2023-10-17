7ce1da2ea61382e6eb13033ff9527595c218cb1c
Michael Lowry <michael.lowry64@gmail.com>
2023-10-17 19:17:50 -0400

    start objective on scene load

--- Assets/Scripts/Objectives/EliminateEnemiesObjective.cs
+++ Assets/Scripts/Objectives/EliminateEnemiesObjective.cs
@@ -4,12 +4,19 @@ public class EliminateEnemiesObjective : ObjectiveManager
 {
     public int totalEnemies; // Total number of enemies at the start of the level
     private int remainingEnemies; // How many enemies remain
+
+    private void Start() 
+    {
+        InitializeObjective();
+    }
+    
     // Override the InitializeObjective method
     public override void InitializeObjective()
     {
         base.InitializeObjective(); // Call parent's initialization
         remainingEnemies = FindObjectsOfType<Enemy>().Length; 
         description = "Eliminate all " + totalEnemies + " enemies on the map";
+        Debug.Log(description);
     }
 
     // Override the CheckObjectiveCompletion method
