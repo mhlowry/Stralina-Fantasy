4850215ce3a3154665020f0996cb5c180a4466c2
Courtney James McCracken II <courtmac.ii@icloud.com>
2023-11-15 00:34:57 -0500

    Added Music files and music to level 5

--- Assets/Scripts/Utility/AudioManager.cs
+++ Assets/Scripts/Utility/AudioManager.cs
@@ -67,6 +67,17 @@ public class AudioManager : MonoBehaviour
         }
         s.source.Play();
     }
+
+    public void Stop(string sound)
+    {
+        Sound s = Array.Find(sounds, item => item.name == sound);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return;
+        }
+        s.source.Stop();
+    }
 }
 
 [Serializable]
---
+++ Assets/Scripts/Utility/MusicManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    [SerializeField] string songName;
+
+    void Start()
+    {
+        AudioManager.instance.Play(songName);
+    }
+
+    void OnDestroy()
+    {
+        AudioManager.instance.Stop(songName);
+    }
+}
