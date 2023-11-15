e9354d75cec1c4a5c1005df5dd53f73e1ea04b2c
Michael Lowry <michael.lowry64@gmail.com>
2023-11-15 00:14:58 -0500

    title menu updated for QA

--- Assets/Scenes/Menus/MenuManager.cs
+++ Assets/Scenes/Menus/MenuManager.cs
@@ -21,6 +21,14 @@ public class MainMenuManager : MonoBehaviour
         usernamePanel.SetActive(true);
     }
 
+    // Temporary function before profiles are implemented
+    public void OnStartGameClicked()
+    {
+        Debug.Log("Start Game Clicked!");
+        // Start coroutine to load the base island scene
+        StartCoroutine(LoadSceneAndSetActive("Base_Scene"));
+    }
+
     public void OnUsernameConfirmClicked()
     {
         Debug.Log("Confirm Clicked!");
--- Assets/Scenes/Menus/TextFadeIn.cs
+++ Assets/Scenes/Menus/TextFadeIn.cs
@@ -1,7 +1,5 @@
 using UnityEngine;
 using TMPro;
-using UnityEngine.UI;
-using UnityEngine.EventSystems;
 using System.Collections;
 
 public class TextAndButtonsFadeIn : MonoBehaviour
@@ -9,26 +7,19 @@ public class TextAndButtonsFadeIn : MonoBehaviour
     public TextMeshProUGUI textToFade; 
     public GameObject buttonsContainer; // Reference to the container with all buttons
     public float fadeInDuration = 3.0f;
-    public Color normalColor = Color.white; // Default color
-    public Color hoverColor = new Color(0.8f, 0.8f, 0.8f); // Off-white color for hover
 
     private void Start()
     {
         // Initialize title to be transparent
-        Color titleColor = textToFade.color;
-        titleColor.a = 0;
-        textToFade.color = titleColor;
+        SetAlpha(textToFade, 0);
 
         // Initialize button texts to be transparent
         foreach (Transform child in buttonsContainer.transform)
         {
-            // Fade text
             TextMeshProUGUI buttonText = child.GetComponentInChildren<TextMeshProUGUI>();
             if (buttonText)
             {
-                Color textColor = buttonText.color;
-                textColor.a = 0;
-                buttonText.color = textColor;
+                SetAlpha(buttonText, 0);
             }
         }
 
@@ -36,71 +27,47 @@ public class TextAndButtonsFadeIn : MonoBehaviour
         StartCoroutine(FadeInSequence());
     }
 
+    private void SetAlpha(TextMeshProUGUI textElement, float alpha)
+    {
+        if (textElement != null)
+        {
+            Color color = textElement.color;
+            color.a = alpha;
+            textElement.color = color;
+        }
+    }
+
     IEnumerator FadeInSequence()
     {
         // Fade in the title text first
-        yield return FadeTitleIn();
+        yield return StartCoroutine(FadeInText(textToFade));
 
         // Now fade in each button text within the ButtonsContainer
         foreach (Transform child in buttonsContainer.transform)
         {
-            yield return FadeButtonIn(child);
+            TextMeshProUGUI buttonText = child.GetComponentInChildren<TextMeshProUGUI>();
+            if (buttonText)
+            {
+                yield return StartCoroutine(FadeInText(buttonText));
+            }
         }
     }
 
-    IEnumerator FadeTitleIn()
+    IEnumerator FadeInText(TextMeshProUGUI textElement)
     {
         float elapsed = 0.0f;
-        Color initialColor = textToFade.color;
+        Color initialColor = textElement.color;
         Color targetColor = initialColor;
-        targetColor.a = 1;  // target alpha is opaque
+        targetColor.a = 1; // target alpha is opaque
 
         while (elapsed < fadeInDuration)
         {
             elapsed += Time.deltaTime;
-            textToFade.color = Color.Lerp(initialColor, targetColor, elapsed / fadeInDuration);
+            textElement.color = Color.Lerp(initialColor, targetColor, elapsed / fadeInDuration);
             yield return null;
         }
 
-        textToFade.color = targetColor;  // ensure fully opaque at the end
-    }
-
-    IEnumerator FadeButtonIn(Transform buttonTransform)
-    {
-        float elapsed = 0.0f;
-
-        TextMeshProUGUI buttonText = buttonTransform.GetComponentInChildren<TextMeshProUGUI>();
-        Color initialTextColor = buttonText.color;
-        Color targetTextColor = initialTextColor;
-        targetTextColor.a = 1;
-
-        while (elapsed < fadeInDuration)
-        {
-            elapsed += Time.deltaTime;
-            buttonText.color = Color.Lerp(initialTextColor, targetTextColor, elapsed / fadeInDuration);
-            yield return null;
-        }
-
-        buttonText.color = targetTextColor;  // ensure fully opaque at the end
-
-        // Add hover functionality after the button text is faded in
-        AddButtonHoverEffect(buttonText);
-    }
-
-    void AddButtonHoverEffect(TextMeshProUGUI buttonText)
-    {
-        EventTrigger eventTrigger = buttonText.gameObject.AddComponent<EventTrigger>();
-        
-        // Pointer Enter event
-        EventTrigger.Entry pointerEnterEntry = new EventTrigger.Entry();
-        pointerEnterEntry.eventID = EventTriggerType.PointerEnter;
-        pointerEnterEntry.callback.AddListener((eventData) => { buttonText.color = hoverColor; });
-        eventTrigger.triggers.Add(pointerEnterEntry);
-        
-        // Pointer Exit event
-        EventTrigger.Entry pointerExitEntry = new EventTrigger.Entry();
-        pointerExitEntry.eventID = EventTriggerType.PointerExit;
-        pointerExitEntry.callback.AddListener((eventData) => { buttonText.color = normalColor; });
-        eventTrigger.triggers.Add(pointerExitEntry);
+        textElement.color = targetColor; // ensure fully opaque at the end
     }
 }
+
