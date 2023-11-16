b0420c7c4981e7152420c9e21ed0f06b1d42e288
Michael Lowry <michael.lowry64@gmail.com>
2023-11-16 14:48:28 -0500

    Dialogue box pt. 3/8
    Account for player input

--- Assets/Scripts/Dialogue/DialogueUI.cs
+++ Assets/Scripts/Dialogue/DialogueUI.cs
@@ -1,11 +1,44 @@
 using UnityEngine;
 using TMPro;
+using System.Collections;
+
 public class DialogueUI : MonoBehaviour
 {
+   [SerializeField] private GameObject dialogueBox;
    [SerializeField] private TMP_Text textLabel;
+   [SerializeField] private DialogueObject testDialogue;
+   private TypewriterEffect typewriterEffect;
+   private Player player;
 
    private void Start()
    {
-        GetComponent<TypewriterEffect>().Run("Welcome to the game!\nHello.", textLabel);
+       player = FindObjectOfType<Player>();
+       typewriterEffect = GetComponent<TypewriterEffect>();
+       CloseDialogueBox();
+       ShowDialogue(testDialogue);
+   }
+
+   public void ShowDialogue(DialogueObject dialogueObject)
+   {
+       dialogueBox.SetActive(true);
+       StartCoroutine(StepThroughDialogue(dialogueObject));
+   }
+
+   private IEnumerator StepThroughDialogue(DialogueObject dialogueObject)
+   {
+       player.DisableInput();
+       foreach (string dialogue in dialogueObject.Dialogue)
+       {
+         yield return typewriterEffect.Run(dialogue, textLabel);
+         yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Space));
+       }
+       CloseDialogueBox();
+       player.EnableInput();
+   }
+
+   private void CloseDialogueBox()
+   {
+       dialogueBox.SetActive(false);
+       textLabel.text = string.Empty;
    }
 }
