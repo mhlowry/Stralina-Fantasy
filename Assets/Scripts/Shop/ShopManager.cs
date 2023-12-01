069a8ffe6170b7eae41fc924fc21ae65547aa097
Courtney James McCracken II <112493851+TheCourtmac@users.noreply.github.com>
2023-12-01 03:30:52 -0500

    Worked on UI for shop

--- Assets/Scripts/Shop/ShopManager.cs
+++ Assets/Scripts/Shop/ShopManager.cs
@@ -1,6 +1,9 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(ShopUIManager))]
 public class ShopManager : MonoBehaviour
 {
     private BoostManager playerBoostManager;
@@ -13,8 +16,23 @@ public class ShopManager : MonoBehaviour
     private List<ShopItem> speedItems;
     private List<ShopItem> funRandomItems;
 
-    void Start()
+    [SerializeField] private List<Sprite> heavyAttackImages;
+    [SerializeField] private List<Sprite> lightAttackImages;
+    [SerializeField] private List<Sprite> hpPointsImages;
+    [SerializeField] private List<Sprite> willpowerImages;
+    [SerializeField] private List<Sprite> speedImages;
+    [SerializeField] private List<Sprite> funRandomImages;
+
+    [SerializeField] Sprite outOfStock;
+
+    PlayerInput playerInput;
+    ShopUIManager uiManager;
+
+    void Awake()
     {
+        uiManager = GetComponent<ShopUIManager>();
+        playerInput = GameObject.Find("PlayerInput").GetComponent<PlayerInput>();
+
         GameObject player = GameObject.FindGameObjectWithTag("Player");
         if (player != null)
         {
@@ -27,68 +45,84 @@ public class ShopManager : MonoBehaviour
         }
 
         InitializeItems();
-        UpdateTotalBoosts();
         DisplayItems();
+        UpdateTotalBoosts();
+
+        //this.gameObject.SetActive(false);
+    }
+
+    private void OnEnable()
+    {
+        playerInput.SwitchCurrentActionMap("UI");
+    }
+
+    private void OnDisable()
+    {
+        playerInput.SwitchCurrentActionMap("Gameplay");
     }
 
     void InitializeItems()
-  {
-      // Heavy Attack Items
-      heavyAttackItems = new List<ShopItem>
-      {
-          new ShopItem("How to Sword 101", ItemType.HeavyAttack, 1, 100),
-          new ShopItem("Hero’s Broken Edge", ItemType.HeavyAttack, 2, 200),
-          new ShopItem("Chaos Twin Blade", ItemType.HeavyAttack, 3, 300)
-      };
+    {
+        // Heavy Attack Items
+        heavyAttackItems = new List<ShopItem>
+        {
+        new ShopItem("How to Sword 101", ItemType.HeavyAttack, 1, 500, heavyAttackImages[0]),
+        new ShopItem("Hero’s Broken Edge", ItemType.HeavyAttack, 2, 1000, heavyAttackImages[1]),
+        new ShopItem("Chaos Twin Blade", ItemType.HeavyAttack, 3, 2000, heavyAttackImages[2])
+        };
 
-      // Light Attack Items
-      lightAttackItems = new List<ShopItem>
-      {
-          new ShopItem("Light Ale", ItemType.LightAttack, 1, 100),
-          new ShopItem("Magic Gloves", ItemType.LightAttack, 2, 200),
-          new ShopItem("Ritual Claw", ItemType.LightAttack, 3, 300)
-      };
+        // Light Attack Items
+        lightAttackItems = new List<ShopItem>
+        {
+        // Assuming lightAttackImages list has corresponding images for these items
+        new ShopItem("Light Ale", ItemType.LightAttack, 1, 500, lightAttackImages[0]),
+        new ShopItem("Magic Gloves", ItemType.LightAttack, 2, 1000, lightAttackImages[1]),
+        new ShopItem("Ritual Claw", ItemType.LightAttack, 3, 2000, lightAttackImages[2])
+        };
 
-      // HP Points Items
-      hpPointsItems = new List<ShopItem>
-      {
-          new ShopItem("Comical Meat", ItemType.HPPoints, 1, 100),
-          new ShopItem("Golden Apple", ItemType.HPPoints, 2, 200),
-          new ShopItem("Heart Vessel", ItemType.HPPoints, 3, 300)
-      };
+        // HP Points Items
+        hpPointsItems = new List<ShopItem>
+        {
+        // Assuming hpPointsImages list has corresponding images for these items
+        new ShopItem("Comical Meat", ItemType.HPPoints, 1, 500, hpPointsImages[0]),
+        new ShopItem("Golden Apple", ItemType.HPPoints, 2, 1000, hpPointsImages[1]),
+        new ShopItem("Heart Vessel", ItemType.HPPoints, 3, 2000, hpPointsImages[2])
+        };
 
-      // Willpower Items
-      willpowerItems = new List<ShopItem>
-      {
-          new ShopItem("Magic Stamp", ItemType.Willpower, 1, 100),
-          new ShopItem("Mythical Medallion", ItemType.Willpower, 2, 200),
-          new ShopItem("Crest of Ram", ItemType.Willpower, 3, 300)
-      };
+        // Willpower Items
+        willpowerItems = new List<ShopItem>
+         {
+        // Assuming willpowerImages list has corresponding images for these items
+        new ShopItem("Magic Stamp", ItemType.Willpower, 1, 500, willpowerImages[0]),
+        new ShopItem("Mythical Medallion", ItemType.Willpower, 2, 1000, willpowerImages[1]),
+        new ShopItem("Crest of Ram", ItemType.Willpower, 3, 2000, willpowerImages[2])
+        };
 
-      // Speed Items
-      speedItems = new List<ShopItem>
-      {
-          new ShopItem("Normal Cookies", ItemType.Speed, 1, 100),
-          new ShopItem("Enchanted Feather", ItemType.Speed, 2, 200),
-          new ShopItem("Strange Boots", ItemType.Speed, 3, 300)
-      };
+        // Speed Items
+        speedItems = new List<ShopItem>
+        {
+        // Assuming speedImages list has corresponding images for these items
+        new ShopItem("Normal Cookies", ItemType.Speed, 1, 500, speedImages[0]),
+        new ShopItem("Enchanted Feather", ItemType.Speed, 2, 1000, speedImages[1]),
+        new ShopItem("Strange Boots", ItemType.Speed, 3, 2000, speedImages[2])
+        };
 
-      // Fun Random Items
-      funRandomItems = new List<ShopItem>
-      {
-          new ShopItem("Potion of Whimsical", ItemType.FunRandom, 0, 150),
-          new ShopItem("Purity Charm", ItemType.FunRandom, 0, 200),
-          new ShopItem("Aka’s Oni Mask", ItemType.FunRandom, 0, 250),
-          new ShopItem("Ao’s Oni Mask", ItemType.FunRandom, 0, 250),
-          new ShopItem("Ic’s Divine Wine", ItemType.FunRandom, 0, 150),
-          new ShopItem("Knife of Memory", ItemType.FunRandom, 0, 300),
-          new ShopItem("Moolon Milk", ItemType.FunRandom, 0, 200),
-          new ShopItem("Ness’s Balanced Breakfast", ItemType.FunRandom, 0, 250),
-          new ShopItem("Unknown’s Eye", ItemType.FunRandom, 0, 350),
-          new ShopItem("Weird Machina", ItemType.FunRandom, 0, 300)
-      };
-  }
+        // Fun Random Items
+        funRandomItems = new List<ShopItem>
+        {
+            new ShopItem("Potion of Whimsical", ItemType.FunRandom, 0, 150, funRandomImages[0]),
+            new ShopItem("Purity Charm", ItemType.FunRandom, 0, 200, funRandomImages[1]),
+            new ShopItem("Aka’s Oni Mask", ItemType.FunRandom, 0, 250, funRandomImages[2]),
+            new ShopItem("Ao’s Oni Mask", ItemType.FunRandom, 0, 250, funRandomImages[3]),
+            new ShopItem("Ic’s Divine Wine", ItemType.FunRandom, 0, 150, funRandomImages[4]),
+            new ShopItem("Knife of Memory", ItemType.FunRandom, 0, 300, funRandomImages[5]),
+            new ShopItem("Moolon Milk", ItemType.FunRandom, 0, 200, funRandomImages[6]),
+            new ShopItem("Ness’s Balanced Breakfast", ItemType.FunRandom, 0, 250, funRandomImages[7]),
+            new ShopItem("Unknown’s Eye", ItemType.FunRandom, 0, 350, funRandomImages[8]),
+            new ShopItem("Weird Machina", ItemType.FunRandom, 0, 300, funRandomImages[9])
+        };
 
+    }
 
     void UpdateTotalBoosts()
     {
@@ -97,67 +131,126 @@ public class ShopManager : MonoBehaviour
                       playerBoostManager.speedBoosts +
                       playerBoostManager.willpowerBoosts +
                       playerBoostManager.healthPointBoosts;
+
+        uiManager.UpdateTotalBoostedText("+"+totalBoosts);
     }
 
     void DisplayItems()
     {
-        ShopUIManager uiManager = GetComponent<ShopUIManager>();
-        if (uiManager == null)
-        {
-            Debug.LogError("ShopUIManager component not found on the GameObject.");
-            return;
-        }
-
+        int lightAttackIndex = playerBoostManager.lightAttackBoosts;
+        uiManager.UpdateLightAttackStatIncreaseText("+" + lightAttackIndex.ToString());
         // Display the next Light Attack Item based on the specific boost level
-        if (playerBoostManager.lightAttackSpecificBoosts < 3)
+        if (lightAttackIndex < 3)
         {
-            int lightAttackIndex = playerBoostManager.lightAttackSpecificBoosts;
             ShopItem lightAttackItem = lightAttackItems[lightAttackIndex];
-            uiManager.UpdateAttackNameText(lightAttackItem.itemName);
-            uiManager.UpdateAttackPriceText(lightAttackItem.purchaseCost.ToString());
+            uiManager.UpdateLightAttackNameText(lightAttackItem.itemName);
+            uiManager.UpdateLightAttackPriceText(lightAttackItem.purchaseCost.ToString());
+            uiManager.UpdateLightAttackImage(lightAttackImages[lightAttackIndex]);
             // Other UI updates for light attack item...
         }
         else
         {
             // Handle cases where 3 specific light attack boosts have been used
-            uiManager.UpdateAttackNameText("No more specific boosts available");
-            uiManager.UpdateAttackPriceText("N/A");
+            uiManager.UpdateLightAttackNameText("Out of Stock!");
+            uiManager.UpdateLightAttackPriceText("N/A");
+            uiManager.UpdateLightAttackImage(outOfStock);
             // Update UI to show that no more specific light attack items are available
         }
 
         // Repeat for other categories using specific boost counters
         // Example for heavy attack:
-        if (playerBoostManager.heavyAttackSpecificBoosts < 3)
+        // Display the next Heavy Attack Item based on the specific boost level
+        int heavyAttackIndex = playerBoostManager.heavyAttackBoosts;
+        uiManager.UpdateHeavyAttackStatIncreaseText("+" + heavyAttackIndex.ToString());
+        if (heavyAttackIndex < 3)
         {
-            int heavyAttackIndex = playerBoostManager.heavyAttackSpecificBoosts;
             ShopItem heavyAttackItem = heavyAttackItems[heavyAttackIndex];
-            uiManager.UpdateDefenseNameText(heavyAttackItem.itemName);
-            uiManager.UpdateDefensePriceText(heavyAttackItem.purchaseCost.ToString());
-            // Update UI for heavy attack item
+            uiManager.UpdateHeavyAttackNameText(heavyAttackItem.itemName);
+            uiManager.UpdateHeavyAttackPriceText(heavyAttackItem.purchaseCost.ToString());
+            uiManager.UpdateHeavyAttackImage(heavyAttackImages[heavyAttackIndex]);
+            // Other UI updates for heavy attack item...
         }
         else
         {
             // Handle cases where 3 specific heavy attack boosts have been used
-            uiManager.UpdateDefenseNameText("No more specific boosts available");
-            uiManager.UpdateDefensePriceText("N/A");
-            // Update UI accordingly
+            uiManager.UpdateHeavyAttackNameText("Out of Stock!");
+            uiManager.UpdateHeavyAttackPriceText("N/A");
+            uiManager.UpdateHeavyAttackImage(outOfStock);
+            // Update UI to show that no more specific heavy attack items are available
         }
 
+
         // Apply similar logic for HP Points, Willpower, Speed
-        // ...
+        // Display the next HP Points Item based on the specific boost level
+        int hpPointsIndex = playerBoostManager.healthPointBoosts;
+        uiManager.UpdateHPPointsStatIncreaseText("+" + hpPointsIndex.ToString());
+        if (playerBoostManager.healthPointBoosts < 3)
+        {
+            ShopItem hpPointsItem = hpPointsItems[hpPointsIndex];
+            uiManager.UpdateHPPointsNameText(hpPointsItem.itemName);
+            uiManager.UpdateHPPointsPriceText(hpPointsItem.purchaseCost.ToString());
+            uiManager.UpdateHPPointsImage(hpPointsImages[hpPointsIndex]);
+            // Other UI updates for HP points item...
+        }
+        else
+        {
+            // Handle cases where 3 specific HP points boosts have been used
+            uiManager.UpdateHPPointsNameText("Out of Stock!");
+            uiManager.UpdateHPPointsPriceText("N/A");
+            uiManager.UpdateHPPointsImage(outOfStock);
+            // Update UI to show that no more specific HP points items are available
+        }
+
+        // Display the next Willpower Item based on the specific boost level
+        int willpowerIndex = playerBoostManager.willpowerBoosts;
+        uiManager.UpdateWillpowerStatIncreaseText("+" + willpowerIndex.ToString());
+        if (playerBoostManager.willpowerBoosts < 3)
+        {
+            ShopItem willpowerItem = willpowerItems[willpowerIndex];
+            uiManager.UpdateWillpowerNameText(willpowerItem.itemName);
+            uiManager.UpdateWillpowerPriceText(willpowerItem.purchaseCost.ToString());
+            uiManager.UpdateWillpowerImage(willpowerImages[willpowerIndex]);
+            // Other UI updates for Willpower item...
+        }
+        else
+        {
+            // Handle cases where 3 specific Willpower boosts have been used
+            uiManager.UpdateWillpowerNameText("Out of Stock!");
+            uiManager.UpdateWillpowerPriceText("N/A");
+            uiManager.UpdateWillpowerImage(outOfStock);
+            // Update UI to show that no more specific Willpower items are available
+        }
+
+        // Display the next Speed Item based on the specific boost level
+        int speedIndex = playerBoostManager.speedBoosts;
+        uiManager.UpdateMoveSpeedStatIncreaseText("+" + speedIndex.ToString());
+        if (playerBoostManager.speedBoosts < 3)
+        {
+            ShopItem speedItem = speedItems[speedIndex];
+            uiManager.UpdateMoveSpeedNameText(speedItem.itemName);
+            uiManager.UpdateMoveSpeedPriceText(speedItem.purchaseCost.ToString());
+            uiManager.UpdateMoveSpeedImage(speedImages[speedIndex]);
+            // Other UI updates for Speed item...
+        }
+        else
+        {
+            // Handle cases where 3 specific Speed boosts have been used
+            uiManager.UpdateMoveSpeedNameText("Out of Stock!");
+            uiManager.UpdateMoveSpeedPriceText("N/A");
+            uiManager.UpdateMoveSpeedImage(outOfStock);
+            // Update UI to show that no more specific Speed items are available
+        }
+
+
 
         // Display a random Fun Random Item
         int randomIndex = Random.Range(0, funRandomItems.Count);
         ShopItem randomItem = funRandomItems[randomIndex];
         uiManager.UpdateRandomItemNameText(randomItem.itemName);
         uiManager.UpdateRandomItemPriceText(randomItem.purchaseCost.ToString());
+        uiManager.UpdateRandomItemImage(funRandomImages[randomIndex]);
         // Update UI for random item
     }
-
-
-
-
-
 
     public void PurchaseItem(ShopItem item)
     {
@@ -168,21 +261,21 @@ public class ShopManager : MonoBehaviour
         }
 
         // Apply the boost from the item to the player
-        ApplyBoost(item);
+        //ApplyBoost(item);
 
         // Update the total boosts and refresh the displayed items
         UpdateTotalBoosts();
         DisplayItems();
     }
 
-    void ApplyBoost(ShopItem item)
+    public void ApplyBoost(ItemType itemType)
     {
         // Logic to apply the boost from the item to the player's stats
         // Example:
-        switch (item.itemType)
+        switch (itemType)
         {
             case ItemType.LightAttack:
-                playerBoostManager.lightAttackBoosts++;
+                playerBoostManager.lightAttackBoosts = Mathf.Clamp(playerBoostManager.lightAttackBoosts + 1, 0, 5);
                 break;
             // Handle other item types similarly
         }
@@ -208,14 +301,16 @@ public class ShopManager : MonoBehaviour
       public ItemType itemType;
       public int boostAmount;
       public int purchaseCost;
+      public Sprite itemImage;
 
       // Constructor
-      public ShopItem(string name, ItemType type, int boost, int cost)
+      public ShopItem(string name, ItemType type, int boost, int cost, Sprite image)
       {
           itemName = name;
           itemType = type;
           boostAmount = boost;
           purchaseCost = cost;
+          itemImage = image;
       }
   }
 
--- Assets/Scripts/Shop/ShopUIManager.cs
+++ Assets/Scripts/Shop/ShopUIManager.cs
@@ -5,72 +5,72 @@ using UnityEngine.UI;
 public class ShopUIManager : MonoBehaviour
 {
     [Header("Price Texts")]
-    public TMP_Text attackPriceText;
-    public TMP_Text defensePriceText;
+    public TMP_Text lightAttackPriceText;
+    public TMP_Text heavyAttackPriceText;
     public TMP_Text moveSpeedPriceText;
-    public TMP_Text attackSpeedPriceText;
-    public TMP_Text knockbackPriceText;
+    public TMP_Text HPPointsPriceText;
+    public TMP_Text willpowerPriceText;
     public TMP_Text randomItemPriceText;
 
     [Header("Item Name Texts")]
-    public TMP_Text attackNameText;
-    public TMP_Text defenseNameText;
+    public TMP_Text lightAttackNameText;
+    public TMP_Text heavyAttackNameText;
     public TMP_Text moveSpeedNameText;
-    public TMP_Text attackSpeedNameText;
-    public TMP_Text knockbackNameText;
+    public TMP_Text HPPointsNameText;
+    public TMP_Text willpowerNameText;
     public TMP_Text randomItemNameText;
 
     [Header("Stat Increase Texts")]
-    public TMP_Text attackStatIncreaseText;
-    public TMP_Text defenseStatIncreaseText;
+    public TMP_Text lightAttackStatIncreaseText;
+    public TMP_Text heavyAttackStatIncreaseText;
     public TMP_Text moveSpeedStatIncreaseText;
-    public TMP_Text attackSpeedStatIncreaseText;
-    public TMP_Text knockbackStatIncreaseText;
+    public TMP_Text HPPointsStatIncreaseText;
+    public TMP_Text willpowerStatIncreaseText;
     public TMP_Text randomItemShuffleText;
 
     [Header("Total Boosted Text")]
     public TMP_Text boostedNumberText;
 
     [Header("Item Images")]
-    public Image attackImage;
-    public Image defenseImage;
+    public Image lightAttackImage;
+    public Image heavyAttackImage;
     public Image moveSpeedImage;
-    public Image attackSpeedImage;
-    public Image knockbackImage;
+    public Image HPPointsImage;
+    public Image willpowerImage;
     public Image randomItemImage;
 
     // Individual methods for updating price text
-    public void UpdateAttackPriceText(string price) { attackPriceText.text = price; }
-    public void UpdateDefensePriceText(string price) { defensePriceText.text = price; }
+    public void UpdateLightAttackPriceText(string price) { lightAttackPriceText.text = price; }
+    public void UpdateHeavyAttackPriceText(string price) { heavyAttackPriceText.text = price; }
     public void UpdateMoveSpeedPriceText(string price) { moveSpeedPriceText.text = price; }
-    public void UpdateAttackSpeedPriceText(string price) { attackSpeedPriceText.text = price; }
-    public void UpdateKnockbackPriceText(string price) { knockbackPriceText.text = price; }
+    public void UpdateHPPointsPriceText(string price) { HPPointsPriceText.text = price; }
+    public void UpdateWillpowerPriceText(string price) { willpowerPriceText.text = price; }
     public void UpdateRandomItemPriceText(string price) { randomItemPriceText.text = price; }
 
     // Individual methods for updating item name text
-    public void UpdateAttackNameText(string name) { attackNameText.text = name; }
-    public void UpdateDefenseNameText(string name) { defenseNameText.text = name; }
+    public void UpdateLightAttackNameText(string name) { lightAttackNameText.text = name; }
+    public void UpdateHeavyAttackNameText(string name) { heavyAttackNameText.text = name; }
     public void UpdateMoveSpeedNameText(string name) { moveSpeedNameText.text = name; }
-    public void UpdateAttackSpeedNameText(string name) { attackSpeedNameText.text = name; }
-    public void UpdateKnockbackNameText(string name) { knockbackNameText.text = name; }
+    public void UpdateHPPointsNameText(string name) { HPPointsNameText.text = name; }
+    public void UpdateWillpowerNameText(string name) { willpowerNameText.text = name; }
     public void UpdateRandomItemNameText(string name) { randomItemNameText.text = name; }
 
     // Individual methods for updating stat increase text
-    public void UpdateAttackStatIncreaseText(string increase) { attackStatIncreaseText.text = increase; }
-    public void UpdateDefenseStatIncreaseText(string increase) { defenseStatIncreaseText.text = increase; }
+    public void UpdateLightAttackStatIncreaseText(string increase) { lightAttackStatIncreaseText.text = increase; }
+    public void UpdateHeavyAttackStatIncreaseText(string increase) { heavyAttackStatIncreaseText.text = increase; }
     public void UpdateMoveSpeedStatIncreaseText(string increase) { moveSpeedStatIncreaseText.text = increase; }
-    public void UpdateAttackSpeedStatIncreaseText(string increase) { attackSpeedStatIncreaseText.text = increase; }
-    public void UpdateKnockbackStatIncreaseText(string increase) { knockbackStatIncreaseText.text = increase; }
+    public void UpdateHPPointsStatIncreaseText(string increase) { HPPointsStatIncreaseText.text = increase; }
+    public void UpdateWillpowerStatIncreaseText(string increase) { willpowerStatIncreaseText.text = increase; }
     public void UpdateRandomItemShuffleText(string shuffle) { randomItemShuffleText.text = shuffle; }
 
     // Method for updating total boosted text
     public void UpdateTotalBoostedText(string boostedNumber) { boostedNumberText.text = boostedNumber; }
 
     // Individual methods for updating item images
-    public void UpdateAttackImage(Sprite sprite) { attackImage.sprite = sprite; }
-    public void UpdateDefenseImage(Sprite sprite) { defenseImage.sprite = sprite; }
+    public void UpdateLightAttackImage(Sprite sprite) { lightAttackImage.sprite = sprite; }
+    public void UpdateHeavyAttackImage(Sprite sprite) { heavyAttackImage.sprite = sprite; }
     public void UpdateMoveSpeedImage(Sprite sprite) { moveSpeedImage.sprite = sprite; }
-    public void UpdateAttackSpeedImage(Sprite sprite) { attackSpeedImage.sprite = sprite; }
-    public void UpdateKnockbackImage(Sprite sprite) { knockbackImage.sprite = sprite; }
+    public void UpdateHPPointsImage(Sprite sprite) { HPPointsImage.sprite = sprite; }
+    public void UpdateWillpowerImage(Sprite sprite) { willpowerImage.sprite = sprite; }
     public void UpdateRandomItemImage(Sprite sprite) { randomItemImage.sprite = sprite; }
 }
