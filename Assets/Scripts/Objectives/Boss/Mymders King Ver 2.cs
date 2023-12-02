280da2511886b5595ba192afd5a4d6469556f897
Michael Lowry <michael.lowry64@gmail.com>
2023-12-01 19:12:36 -0500

    Bye Mimexos!!!!!

--- Assets/Scripts/Objectives/Boss/Mymders King Ver 2.cs
+++ Assets/Scripts/Objectives/Boss/Mymders King Ver 2.cs
@@ -1,9 +1,9 @@
 using UnityEngine;
 using System.Collections;
 
-public class MimexosKingVer2 : ObjectiveManager
+public class MymdersKingVer2 : ObjectiveManager
 {
-    private bool isMimexosKingDestroyed = false;
+    private bool isMymdersKingDestroyed = false;
 
     private void Start()
     {
@@ -22,37 +22,37 @@ public class MimexosKingVer2 : ObjectiveManager
 
     private void HandleEnemyDestroyed()
     {
-        // Check if Mimexos King Ver 2 is still present in the scene
-        Enemy mimexosKing = FindMimexosKingInScene();
-        if (mimexosKing == null) // If not found, it means it has been destroyed
+        // Check if Mymders King Ver 2 is still present in the scene
+        Enemy MymdersKing = FindMymdersKingInScene();
+        if (MymdersKing == null) // If not found, it means it has been destroyed
         {
-            isMimexosKingDestroyed = true;
-            Debug.Log("Mimexos King Ver 2 destroyed.");
+            isMymdersKingDestroyed = true;
+            Debug.Log("Mymders King Ver 2 destroyed.");
             CheckObjectiveCompletion();
         }
     }
 
-    private Enemy FindMimexosKingInScene()
+    private Enemy FindMymdersKingInScene()
     {
-        // Search for Mimexos King Ver 2 in the scene
+        // Search for Mymders King Ver 2 in the scene
         Enemy[] allEnemies = FindObjectsOfType<Enemy>();
         foreach (Enemy enemy in allEnemies)
         {
-            if (enemy.name == "Mimexos King Ver 2" && !enemy.GetIsDead())
+            if (enemy.name == "Mymders King Ver 2" && !enemy.GetIsDead())
             {
-                Debug.Log($"Mimexos King Ver 2 found and is alive: {enemy.name}");
+                Debug.Log($"Mymders King Ver 2 found and is alive: {enemy.name}");
                 return enemy;
             }
         }
-        Debug.Log("Mimexos King Ver 2 not found or is dead.");
-        return null; // Return null if Mimexos King Ver 2 is not found or is dead
+        Debug.Log("Mymders King Ver 2 not found or is dead.");
+        return null; // Return null if Mymders King Ver 2 is not found or is dead
     }
 
 
     public override void CheckObjectiveCompletion()
     {
         Debug.Log("Checking objective completion.");
-        if (isMimexosKingDestroyed)
+        if (isMymdersKingDestroyed)
         {
             CompleteObjective();
         }
@@ -60,13 +60,13 @@ public class MimexosKingVer2 : ObjectiveManager
 
     protected override void UpdateObjectiveDescription()
     {
-        if (isMimexosKingDestroyed)
+        if (isMymdersKingDestroyed)
         {
-            description = "Mimexos King Ver 2 has been defeated!";
+            description = "Mymders King Ver 2 has been defeated!";
         }
         else
         {
-            description = "Defeat the Mimexos King!";
+            description = "Defeat the Mymders King!";
         }
         Debug.Log(description);
         base.UpdateObjectiveDescription();
